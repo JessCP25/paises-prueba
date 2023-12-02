e1585e22ed7feb492a4f7700d83836fa962c06b8
JessCP25 <jpayanoc25@gmail.com>
2023-12-02 07:39:53 -0500

    Configurando las rutas de la aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { Vista1PageComponent } from './pages/vista1-page/vista1-page.component';
+import { Vista2PageComponent } from './pages/vista2-page/vista2-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomePageComponent
+    },
+    {
+        path: 'vista1',
+        component: Vista1PageComponent
+    },
+    {
+        path: 'vista2',
+        component: Vista2PageComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
